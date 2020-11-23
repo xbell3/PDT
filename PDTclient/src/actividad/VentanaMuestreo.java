db175d3d63ed6f1ea1c084dd438cf9b33a0a2ca0
Usuario <Usuario@Usuario-PC>
2020-11-22 21:53:57 -0300

    Visual VentanaMuestreo

--- PDTclient/src/actividad/VentanaMuestreo.java
+++ PDTclient/src/actividad/VentanaMuestreo.java
@@ -7,23 +7,118 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import com.cliente.VentanaGeneral;
+import com.cliente.VentanaInicio;
 import com.entidades.Formulario;
 import com.entidades.Usuario;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollBar;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class VentanaMuestreo extends JFrame {
 
 	private JPanel contentPane;
-
 	/**
 	 * Create the frame.
 	 */
 	public VentanaMuestreo(Usuario usuario, Formulario formulario) {
+		
+		setTitle("Actividad");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaActividad.class.getResource("/Imagenes/iAGRO_V04.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JPanel panelUsuario = new JPanel();
+		panelUsuario.setLayout(null);
+		panelUsuario.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, UIManager.getColor("Button.light"),
 
+						UIManager.getColor("Button.shadow"), null, null));
+		panelUsuario.setBackground(new Color(0, 102, 0));
+		panelUsuario.setBounds(0, 0, 624, 90);
+		contentPane.add(panelUsuario);
+		
+		JLabel lblTipoUser = new JLabel("TipoUser");
+		lblTipoUser.setBounds(453, 45, 46, 14);
+		panelUsuario.add(lblTipoUser);
+		
+		JLabel lblNewLabel = new JLabel("(Nombre del usuario)");
+		lblNewLabel.setBounds(400, 20, 118, 14);
+		panelUsuario.add(lblNewLabel);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				VentanaInicio ventanaInicio = new VentanaInicio(usuario);
+				ventanaInicio.setLocation(400, 150);
+				ventanaInicio.setVisible(true);
+				dispose();
+			}
+		});
+		
+		JButton btnNewButton = new JButton("Ayuda");
+		btnNewButton.setBounds(528, 16, 88, 23);
+		panelUsuario.add(btnNewButton);
+		
+		JLabel lblNombreSistema = new JLabel("Nombre App");
+		lblNombreSistema.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNombreSistema.setBounds(10, 16, 152, 34);
+		panelUsuario.add(lblNombreSistema);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(VentanaActividad.class.getResource("/Imagenes/klipartz.com.png")));
+		lblNewLabel_1.setBounds(-94, 0, 718, 90);
+		panelUsuario.add(lblNewLabel_1);
+		
+		JPanel panelActividad = new JPanel();
+		panelActividad.setLayout(null);
+		panelActividad.setBackground(new Color(204, 255, 204));
+		panelActividad.setBounds(0, 89, 624, 472);
+		contentPane.add(panelActividad);
+		
+		JLabel lblTituloFormulario = new JLabel("Actividad de campo");
+		lblTituloFormulario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTituloFormulario.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblTituloFormulario.setBounds(10, 11, 171, 39);
+		panelActividad.add(lblTituloFormulario);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				VentanaIniciarActividad ventanaIniciarActividad = new VentanaIniciarActividad(usuario);
+				ventanaIniciarActividad.setVisible(true);
+				ventanaIniciarActividad.setLocation(400, 150);
+				dispose();
+			}
+		});
+		btnVolver.setBounds(10, 438, 89, 23);
+		panelActividad.add(btnVolver);
+		
+		JLabel lblBienvenido = new JLabel("Bienvenido al centro de actividad de campo.");
+		lblBienvenido.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblBienvenido.setBounds(20, 57, 391, 39);
+		panelActividad.add(lblBienvenido);
+		
+	}
 }
